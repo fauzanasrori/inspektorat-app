e25d31778ba6dc5503510a0e6d6d8ff42ff53ce6
Asrori87 <fauzanasrori87@gmail.com>
2025-04-20 18:10:42 +0700

    feat: mempersiapkan kerangkan nextjs inspektorat

--- supabase.js
+++
@@ -1,6 +0,0 @@
-const { createClient } = require("@supabase/supabase-js");
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const SupabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-const supabase = createClient(supabaseUrl, SupabaseKey);
